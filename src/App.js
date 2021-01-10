4a7a057161ed8d907b91f45cf4757cd8a9d64c39
unknown <sadiqajimustapha@gmail.com>
2021-01-09 17:37:42 -0800

    Finished the coming soon page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Calendar from "./assests/images/Calendar.png"
+import twitter from "./assests/images/twitter.png"
+import instagram from "./assests/images/instagram.png"
+import facebook from "./assests/images/facebook.png"
+import dribbble from "./assests/images/dribbble.png"
+
+const social = [
+  {icon: twitter, to: "twitter.com/#", alt: "Twitter"},
+  {icon: instagram, to: "instagram.com/#", alt: "Instagram"},
+  {icon: facebook, to: "facebook.com/#", alt: "Facebook"},
+  {icon: dribbble, to: "dribbble.com/#", alt: "Dribbble"}
+
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <p style={{textTransform: "uppercase"}}>Coming Soon</p>
+        <p>A creative marketplace where indivduals can make money<br />
+from selling their art to the world!</p>
+        <h1>Get notified<br />
+when we launch 🚀</h1>
+        <div>
+          <input style={{height: 50, paddingLeft: 10}} placeholder="Email address"/>
+          <button style={{height: 55, marginLeft: 10}}>
+            Okay, I’m in!
+        </button>
+        </div>
+        <div style={{marginTop: 20, marginLeft: 10, width: "50%", display: "flex", justifyContent: "space-between"}}>
+          {social.map(({icon,alt, to}) => (
+            <a href={to} >
+              <img src={icon} alt={alt}/>
+            </a>
+          ))}
+        </div>
+      </div>
+
+      <div >
+        <img src={Calendar} alt="calendar" />
+      </div>
+
     </div>
   );
 }
