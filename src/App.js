2b6e6ef5af70a353ea83a4055f175f9b24f660c8
OkoroEugene <obinna.okoroeugene@gmail.com>
2021-01-18 16:17:19 +0100

    fix bug with subscribe

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 import Calendar from "./assests/images/Calendar.png"
 import twitter from "./assests/images/twitter.png"
 import instagram from "./assests/images/instagram.png"
@@ -28,7 +30,26 @@ const social = [
 ]
 
 function App() {
-  const [processing, setProcrssing] = useState(false);
+  const [processing, setProcessing] = useState(false);
+  const [email, setEmail] = useState("");
+
+  const subscribe = async () => {
+    setProcessing(true)
+
+    try {
+      const proxyurl = "https://cors-anywhere.herokuapp.com/";
+      const url = "https://boatisland-api.herokuapp.com/api/subscribe";
+      const res = await axios.post(url, { email });
+
+      if (res.data) {
+        toast(res.data.message, { type: "success" });
+      }
+    } catch (error) {
+      console.log(error.response)
+      toast(error.response.data.message, { type: "success" });
+    }
+    setProcessing(false);
+  }
 
   return (
     <div className="container">
@@ -38,24 +59,27 @@ function App() {
             <img src={Calendar} alt="calendar" />
           </div>
           <p style={{ textTransform: "uppercase" }}>Coming Soon</p>
-          <p>A creative marketplace where indivduals can make money<br />
-from selling their art to the world!</p>
-          <h1>Get notified<br />
-when we launch 🚀</h1>
+          <p>A creative marketplace where indivduals can make money<br />from selling their art to the world!</p>
+          <h1>Get notified<br />when we launch 🚀</h1>
           <div>
-            <input className="email" style={{ height: 50, paddingLeft: 10 }} placeholder="Email address" />
+            <input
+              className="email"
+              style={{ height: 50, paddingLeft: 10 }}
+              placeholder="Email address"
+              onChange={e => setEmail(e.target.value)}
+            />
             <button
-              onClick={() => setProcrssing(true)}
+              onClick={subscribe}
               disabled={processing}
               className="btn-subscribe"
               style={{ height: 55, marginLeft: 10, cursor: "pointer" }}>
               {!processing && <span>Okay, I’m in!</span>}
-              {processing && <div class="">Processing...</div>}
+              {processing && <div className="">Processing...</div>}
         </button>
           </div>
           <div style={{ marginTop: 20, marginLeft: 10, width: "50%", display: "flex", justifyContent: "space-between" }}>
             {social.map(({ icon, alt, to }) => (
-              <a href={to} target="_blank" >
+              <a key={alt} href={to} target="_blank" >
                 <img src={icon} alt={alt} />
               </a>
             ))}
@@ -75,9 +99,9 @@ when we launch 🚀</h1>
             <path d="M9.7666 2.23333C9.20782 1.66951 8.52952 1.23841 7.7818 0.971859C7.03408 0.70531 6.23604 0.610125 5.4466 0.693332C2.99993 0.939999 0.986599 2.92667 0.713266 5.37333C0.3466 8.60667 2.8466 11.3333 5.99993 11.3333C7.00667 11.3333 7.99274 11.0477 8.84359 10.5096C9.69444 9.97143 10.3751 9.20293 10.8066 8.29333C11.0199 7.84667 10.6999 7.33333 10.2066 7.33333C9.95993 7.33333 9.7266 7.46667 9.61993 7.68667C9.23287 8.51935 8.57084 9.19332 7.74521 9.59521C6.91958 9.99709 5.98073 10.1024 5.0866 9.89333C3.6066 9.56667 2.41327 8.36 2.09993 6.88C1.96822 6.29484 1.96975 5.68756 2.1044 5.10306C2.23905 4.51857 2.50338 3.97183 2.87783 3.50328C3.25229 3.03473 3.7273 2.65635 4.26771 2.39614C4.80813 2.13593 5.40013 2.00055 5.99993 2C7.1066 2 8.09327 2.46 8.81326 3.18667L7.8066 4.19333C7.3866 4.61333 7.67993 5.33333 8.27327 5.33333H10.6666C11.0333 5.33333 11.3333 5.03333 11.3333 4.66667V2.27333C11.3333 1.68 10.6133 1.38 10.1933 1.8L9.7666 2.23333Z" fill="url(#paint0_linear)" />
             <defs>
               <linearGradient id="paint0_linear" x1="-8.34722" y1="4.89227" x2="5.47578" y2="18.2277" gradientUnits="userSpaceOnUse">
-                <stop stop-color="#B33BF6" />
-                <stop offset="0.648958" stop-color="#F15223" />
-                <stop offset="1" stop-color="#FF844C" />
+                <stop stopColor="#B33BF6" />
+                <stop offset="0.648958" stopColor="#F15223" />
+                <stop offset="1" stopColor="#FF844C" />
               </linearGradient>
             </defs>
           </svg>
@@ -112,9 +136,9 @@ when we launch 🚀</h1>
             <path d="M9.7666 2.23333C9.20782 1.66951 8.52952 1.23841 7.7818 0.971859C7.03408 0.70531 6.23604 0.610125 5.4466 0.693332C2.99993 0.939999 0.986599 2.92667 0.713266 5.37333C0.3466 8.60667 2.8466 11.3333 5.99993 11.3333C7.00667 11.3333 7.99274 11.0477 8.84359 10.5096C9.69444 9.97143 10.3751 9.20293 10.8066 8.29333C11.0199 7.84667 10.6999 7.33333 10.2066 7.33333C9.95993 7.33333 9.7266 7.46667 9.61993 7.68667C9.23287 8.51935 8.57084 9.19332 7.74521 9.59521C6.91958 9.99709 5.98073 10.1024 5.0866 9.89333C3.6066 9.56667 2.41327 8.36 2.09993 6.88C1.96822 6.29484 1.96975 5.68756 2.1044 5.10306C2.23905 4.51857 2.50338 3.97183 2.87783 3.50328C3.25229 3.03473 3.7273 2.65635 4.26771 2.39614C4.80813 2.13593 5.40013 2.00055 5.99993 2C7.1066 2 8.09327 2.46 8.81326 3.18667L7.8066 4.19333C7.3866 4.61333 7.67993 5.33333 8.27327 5.33333H10.6666C11.0333 5.33333 11.3333 5.03333 11.3333 4.66667V2.27333C11.3333 1.68 10.6133 1.38 10.1933 1.8L9.7666 2.23333Z" fill="url(#paint0_linear)" />
             <defs>
               <linearGradient id="paint0_linear" x1="-8.34722" y1="4.89227" x2="5.47578" y2="18.2277" gradientUnits="userSpaceOnUse">
-                <stop stop-color="#B33BF6" />
-                <stop offset="0.648958" stop-color="#F15223" />
-                <stop offset="1" stop-color="#FF844C" />
+                <stop stopColor="#B33BF6" />
+                <stop offset="0.648958" stopColor="#F15223" />
+                <stop offset="1" stopColor="#FF844C" />
               </linearGradient>
             </defs>
           </svg>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={true}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
